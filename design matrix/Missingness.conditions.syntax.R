2760fa12c5630c86323e6a95e952a446b60bd01f
Zino Brystowski <125873161+ZinoBr@users.noreply.github.com>
2023-03-02 16:33:11 +0100

    Update Missingness.conditions.syntax.R

--- design matrix/Missingness.conditions.syntax.R
+++ design matrix/Missingness.conditions.syntax.R
@@ -13,6 +13,7 @@ missing_view <- list(1,
                      c(3,4),
                      c(1,2,3),
                      c(1,2,4),
+                     c(1,3,4),
                      c(2,3,4),
                      c(1,2,3,4))
 
@@ -44,13 +45,11 @@ missing_prop <-   list(   c(0.3),
 )
 
 # All possible combinations of view selection and missing proportions under the condition n views = n missing prop.
-# NOTE: The condition for all views containing missingness was is excluded, see line 53
-
 miss.conditions <- rbind(
   expand.grid( missing_view [1:4],  missing_prop [1:2]),
   expand.grid( missing_view [5:10], missing_prop [3:6]),
-  expand.grid( missing_view [11:13], missing_prop [7:12])
-#  ,expand.grid( missing_view [14],  missing_prop [13:24])
+  expand.grid( missing_view [11:14], missing_prop [7:12]),
+  expand.grid( missing_view [15],  missing_prop [13:24])
 )
 
 # Change names of list objects
