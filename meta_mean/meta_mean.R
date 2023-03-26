6c2e7f27bda48667e0bd24bb372e2ced7f78141b
Zino Brystowski <125873161+ZinoBr@users.noreply.github.com>
2023-03-26 22:26:05 +0200

    Update meta_mean.R
    The line for saving the coefficients was misspecified

--- meta_mean/meta_mean.R
+++ meta_mean/meta_mean.R
@@ -110,7 +110,7 @@ results$missing_prop <- miss.conditions$missing_prop[ conds$index.misscond[TID] 
 results$missing_view <- miss.conditions$missing_view[ conds$index.misscond[TID] ]
 results$noise_view <- conds$noise_view[TID]
 results$preds <- predict(meta_mean_fit, complete_data$xtest)
-results$coefs <- coef(meta_mean_fit)$`Level 2`[[1]][-1]
+results$coefs <-  coef(meta_mean_fit)$`meta`[-1]
 results$ytest <- complete_data$ytest 
 results$ptest <- complete_data$ptest
 results$time <- time
