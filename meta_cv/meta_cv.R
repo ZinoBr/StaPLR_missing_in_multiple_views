4a7d371bb64c923953d08134f7151aaf152ab52b
Zino Brystowski <125873161+ZinoBr@users.noreply.github.com>
2023-03-26 22:24:48 +0200

    Update meta_cv.R
    The line for saving the coefficients was misspecified

--- meta_cv/meta_cv.R
+++ meta_cv/meta_cv.R
@@ -109,7 +109,7 @@ results$missing_prop <- miss.conditions$missing_prop[ conds$index.misscond[TID] 
 results$missing_view <- miss.conditions$missing_view[ conds$index.misscond[TID] ]
 results$noise_view <- conds$noise_view[TID]
 results$preds <- predict(meta_cv_fit, complete_data$xtest)
-results$coefs <- coef(meta_cv_fit)$`Level 2`[[1]][-1]
+results$coefs <-  coef(meta_cv_fit)$`meta`[-1]
 results$ytest <- complete_data$ytest 
 results$ptest <- complete_data$ptest
 results$time <- time
