83967ae5f5935be473444ea02e3de1f774626082
Zino Brystowski <125873161+ZinoBr@users.noreply.github.com>
2023-07-24 21:43:32 +0200

    Deletion of design matrix syntax for replacement
    A revised syntax for creating the design matrix is uploaded. The difference to the previous syntax is that here the grid expansion function and the increasing sequences rule are integrated. The result is the same.

--- design matrix/transform.listobj.to.vector.R
+++
@@ -1,3 +0,0 @@
-# Function to transform list element to a vector when 'as.vector' does not work
-transform.listobj.to.vector <-
-  function( i ) {  as.vector(   t(  as.matrix(  as.data.frame( i )  )))  }
